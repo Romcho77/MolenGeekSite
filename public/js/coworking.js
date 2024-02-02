8fd6a341d92d28910513648473fae7073554b972
unknown <ryzhonkovr@gmail.com>
2024-02-02 10:43:26 +0100

    Adding 3d page coworking

--- public/js/coworking.js
+++ public/js/coworking.js
@@ -1,15 +1,16 @@
 import DATA from "./../JSON/molengeek.json" assert{type:"json"};
 
-let bottone = document.querySelector("#bottone1")
+let bottone1 = document.querySelector("#bottone1")
+let bottone2 = document.querySelector("#bottone2")
 let section1 = document.querySelector(".section1")
 let section2 = document.querySelector(".section2")
-let buttonReturn = document.querySelector(".buttonReturn")
+let section3 = document.querySelector(".section33")
+let buttonReturn1 = document.querySelector(".buttonReturn1")
+let buttonReturn2 = document.querySelector(".buttonReturn2")
+
+
 
 // ! Button to Top
-
-
-// When the user scrolls down 20px from the top of the document, show the button
-
 
 let goTop = document.querySelector("button")
 goTop.addEventListener("click",()=>{
@@ -19,50 +20,34 @@ goTop.addEventListener("click",()=>{
 
 // ! Button to Top
 
-
-
-
-//? buttons to change section 1 and 2 
-bottone.addEventListener('click',()=>{
+// ? section 1>2
+bottone1.addEventListener('click',()=>{
     section1.style.display = "none"
     section2.style.display = "block"
-    buttonReturn.style.display = "block"
+    buttonReturn1.style.display = "block"
 
 })
 
-buttonReturn.addEventListener("click",()=>{
+buttonReturn1.addEventListener("click",()=>{
     section1.style.display = "block"
-    // section2.style.display = "none"
-    buttonReturn.style.display = "none"
+    section2.style.display = "none"
+    buttonReturn1.style.display = "none"
 })
 
-    
+// ? section 1>3
+bottone2.addEventListener('click',()=>{
+    section1.style.display = "none"
+    section3.style.display = "block"
+    buttonReturn2.style.display = "block"
 
-//? buttons to change section 1 and 2 
+})
 
+buttonReturn2.addEventListener("click",()=>{
+    section1.style.display = "block"
+     section3.style.display = "none"
+    buttonReturn2.style.display = "none"
+})
 
-let seanceInfo = DATA.formations.seancesInfos
-let section4 = document.querySelector(".section4")
-
-let div
-let p
-let span
-
-for (const key of seanceInfo) {
-    div = document.createElement("div")
-    div.setAttribute("class","event1 center")
-    span = document.createElement("span")
-    p = document.createElement("p")
-    p.setAttribute("class","eventText center")
-    span.setAttribute("class","eventDate center")
-    p.innerText = key.nom
-    span.innerText = key.date
-    div.appendChild(span)
-    div.appendChild(p)
-    section4.appendChild(div)
-        
-    
-}
 
 let formulaire = DATA.molengeekInfo
 let section6Right = document.querySelector(".sec6Right")
@@ -71,7 +56,9 @@ let section6Right = document.querySelector(".sec6Right")
 //     console.log(Object.values(iterator).split(",", " "));
 //     // console.log(iterator.valueOf(iterator));
 // }
-
+let div
+let p
+let span
 for (const info in formulaire) {
     console.log(formulaire[info]);
     p = document.createElement("p")
@@ -89,7 +76,3 @@ for (const image in partenaires) {
     slides[counter].querySelector("img").src = partenaires[image].logo
     counter++
 }
-
-
-
-
